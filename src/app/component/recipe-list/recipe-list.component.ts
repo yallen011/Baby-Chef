a64d4622c6b34f0340fe696938eb6aa4411ab211
Yvonne Allen <yallen011@gmail.com>
2018-07-30 20:19:20 -0400

    moved recipe item code into recipe-item component and added an input property to recipe-item to pass recipe data

--- src/app/component/recipe-list/recipe-list.component.ts
+++ src/app/component/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
     name: 'Shrimp Salad',
     description: 'juicy shrimp with a full bed of lettuce and veggies',
     imagePath: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'
+  },
+  {
+    name: 'Quinoa',
+    description: 'hearty meal with green peas and chopped carrots',
+    imagePath: 'https://cdn.pixabay.com/photo/2017/09/17/23/21/gastronomy-2760200_1280.jpg'
   }];
 
   constructor() { }
