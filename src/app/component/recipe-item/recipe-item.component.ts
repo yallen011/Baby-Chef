abbc8141756964c37c0552a8eb84d933e1e1216b
Yvonne Allen <yallen011@gmail.com>
2018-12-10 15:27:56 -0500

    changed recipe item to emit an event with no data since you can send the selected recipe from the for loop

--- src/app/component/recipe-item/recipe-item.component.ts
+++ src/app/component/recipe-item/recipe-item.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../../model/recipe';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipe: Recipe;
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
+  @Output() selectedRecipe = new EventEmitter<void>();
 
   constructor() { }
 
@@ -17,7 +17,7 @@ export class RecipeItemComponent implements OnInit {
   }
 
   onSelectRecipe() {
-    this.selectedRecipe.emit(this.recipe);
+    this.selectedRecipe.emit();
   }
 
 }
