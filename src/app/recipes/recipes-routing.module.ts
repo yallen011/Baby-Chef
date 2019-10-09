7c1a611cb176ce3abe8b0c04be51ea19b7fda6f2
Yvonne Allen <yallen011@gmail.com>
2019-10-09 19:37:49 -0400

    added recipe module with routing to app module.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'recipes', component: RecipeBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
